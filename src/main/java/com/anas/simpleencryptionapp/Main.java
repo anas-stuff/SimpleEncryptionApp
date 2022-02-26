c517ca38c64f07fb4773f9e60e225d475a7872b9
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-02-26 20:20:34 +0200

    Add main class 😁▶

---
+++ src/main/java/com/anas/simpleencryptionapp/Main.java
@@ -0,0 +1,9 @@
+package com.anas.simpleencryptionapp;
+
+import com.anas.simpleencryptionapp.gui.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        new MainFrame("Simple encryption app");
+    }
+}
